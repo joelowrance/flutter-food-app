d6ecdd20ba771234f5841859dd0597bf1fbaae79
Joe <joelowrance@gmail.com>
2022-12-12 19:36:59 -0500

    Add alert dialog

---
+++ sellers/lib/widgets/error_dialog.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class ErrorDialog extends StatelessWidget {
+  final String? message;
+
+  ErrorDialog({this.message}) {}
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      key: key,
+      content: Text(message!),
+      actions: [
+        ElevatedButton(
+          child: Center(child: Text("OK")),
+          style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        )
+      ],
+    );
+  }
+}
