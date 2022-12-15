e8ce4e9b3fc0c2adbe01830753a97f9835e69abf
Joe <joelowrance@gmail.com>
2022-12-14 19:36:15 -0500

    Fix firebase init, add loading widget

--- sellers/lib/main.dart
+++ sellers/lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:sellers_app/firebase_options.dart';
 import 'package:sellers_app/splashScreen/splash_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  //await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
