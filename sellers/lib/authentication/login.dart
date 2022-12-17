1a2bbeef16abea76edd359b215b18def736405b7
Joe <joelowrance@gmail.com>
2022-12-17 14:44:17 -0500

    Add login in functionality

--- sellers/lib/authentication/login.dart
+++ sellers/lib/authentication/login.dart
@@ -1,5 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:sellers_app/global/global.dart';
+import 'package:sellers_app/mainScreens/home_screen.dart';
 import 'package:sellers_app/widgets/custom_text_field.dart';
+import 'package:sellers_app/widgets/error_dialog.dart';
+import 'package:sellers_app/widgets/loading_dialog.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -12,6 +18,55 @@ class _LoginScreenState extends State<LoginScreen> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+
+  formValidation() {
+    if (emailController.text.isNotEmpty && passwordController.text.isNotEmpty) {
+      loginNow();
+    } else {
+      showDialog(
+          context: context,
+          builder: (c) {
+            return const ErrorDialog(message: "Please enter email and password.");
+          });
+    }
+  }
+
+  void loginNow() async {
+    showDialog(
+        context: context,
+        builder: (c) {
+          return const LoadingDialog(message: "Validating Credentials.");
+        });
+
+    User? currentUser;
+    await firebaseAuth
+        .signInWithEmailAndPassword(email: emailController.text.trim(), password: passwordController.text.trim())
+        .then((auth) => {currentUser = auth.user})
+        .catchError((error) {
+      Navigator.pop(context);
+      showDialog(
+          context: context,
+          builder: (c) {
+            return ErrorDialog(message: error.message.toString());
+          });
+    });
+
+    if (currentUser != null) {
+      readDataAndSetLocalData(currentUser!).then((value) {
+        Navigator.pop(context);
+        Navigator.push(context, MaterialPageRoute(builder: (c) => const HomeScreen()));
+      });
+    }
+  }
+
+  Future<void> readDataAndSetLocalData(User currentUser) async {
+    await FirebaseFirestore.instance.collection("sellers").doc(currentUser.uid).get().then((snapshot) async {
+      await sharedPreferences!.setString("uid", currentUser.uid);
+      await sharedPreferences!.setString("email", snapshot.data()!["sellerEmail"]); //emailcontroller
+      await sharedPreferences!.setString("name", snapshot.data()!["sellerName"]);
+      await sharedPreferences!.setString("photoUrl", snapshot.data()!["sellerAvatarUrl"]);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -52,15 +107,14 @@ class _LoginScreenState extends State<LoginScreen> {
             height: 30,
           ),
           ElevatedButton(
-            onPressed: () => print("clicked"),
+            onPressed: () {
+              formValidation();
+            },
             style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.cyan,
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 50, vertical: 20)),
+                backgroundColor: Colors.cyan, padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 20)),
             child: const Text(
               "Log In",
-              style:
-                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
             ),
           ),
         ],
